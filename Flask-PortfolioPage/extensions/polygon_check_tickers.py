4c763020f1ecd1d69fc5b28342bca61c03c746fd
dpdanieldp <92707909+dpdanieldp@users.noreply.github.com>
2021-11-08 17:05:55 +0100

    Update polygon_check_tickers.py

--- Flask-PortfolioPage/extensions/polygon_check_tickers.py
+++ Flask-PortfolioPage/extensions/polygon_check_tickers.py
@@ -6,40 +6,7 @@ from website import models
 
 
 def polygon_check(ticker_list):
-    api_keys_list = ['yUSniyZhsOS5mL5kd9N0wSDXFrl0poAI', 'clsyK9TpKDfqel1gMO3u64UnO3SL9dUR',
-                     'tV7mNlNS4lGGi8P7qB7rNuF3s4d6jsoR', 'pmhrv2Mzh_bxcLAEmJCDyjY5oRpCIBoy',
-                     'lwBZG7QTOdKmuGhTM60bd3hNMpU8AYIw', 'TVRChL8pSAoZ3xpl_127sNTcZZru5lTb',
-                     'tKRCf11vHdJhO9EUgCZRuK2lpaLYhXdG',
-                     'CgPEp6ob8oRAthZlPFWykjuzBDEqp2si',
-                     'yBE2Jn3kgoaMQqowqH3lovpUK9qU0W76',
-                     'iEF3AkM55DuCRIrSVj2rkzH9HvDjcEYD',
-                     '8S7LmmmuEPsYCJ0ngZGmw9Hr3TaUSYqM',
-                     'SYs6fkbCn89t4thjz8_9edAuOq40HylA',
-                     'YvCEfNq4Xl8MIzUDM60A9yUqwiA2zIsf',
-                     '3PBxJmsIFpGKvz1wc34XuNZwPW9L71Mo',
-                     'Z9ltuMdUStyjqmlDbg1k2kXThovXiMpq',
-                     'RmTMYJ6XT7RIGxM2VpI_9r7YOu43hIuA',
-                     'TX4lqCH5NEPb5GtZZle0SFykp7ng0XH6',
-                     'o4BHWBPKwMyaovIlnr83xoBufgGgbcNr',
-                     'GZotuQuN5Hp5_BoPxTo33ITgELx_z1pE',
-                     'kB0bwXRSzPSEVCtTp2jIniaZGfurlQWx',
-                     'Tw24ZhBgm4AOkJR0Z06w7GCkyWvtDLZ2',
-                     '0s5OQQklJIesir5zAfYVNIFvQdGt5LFh',
-                     'gB2au2i2yWNNUIwkjLdpKiKDjyl06Y7j',
-                     'id5gZISrUDMUjnVT4d6TYpgQhw3f55ka',
-                     'mw2LWPLjZzKjUSdiIvOvP25fwI5qobLX',
-                     'zPyPnkHZI6pC1ATbQl8wxSO9GLYIpsCp',
-                     'CdKxkCk437ZZ6oMnC2yGzcGSyH8GYBZ4',
-                     'ArfGTdWXUtj4Vjl1xvm2Gwzm6V7Hpkmc',
-                     'GLTQWikvO4pTSlsK0l87CXCWfg8AP7yT',
-                     'bw4ypp8TSg6xjClDofdKUcSEHnfChnkZ',
-                     'quNWRFeMZd4VL5NukpfxbZO3ksnwHwbV',
-                     '7fmhhklP_9xKzP2ecvy3SsYxIF_P3t3L',
-                     'A4i67lX7CTgkdv5rpqjpFSHx9dF9N6ih',
-                     'aHk7_Oi5DnTliLxGJJJBVeC4ZeVSSI8r',
-                     'bwkAptOSHDkelEmr9sg0EgX2ok_Mjuaw',
-                     'OL3DLjRrnBEPQaS1zw99iLf1p7Oek_Sq',
-                     'TlWgewHnHLwkRQ2HpYTXIyKP4H7bqccE']
+    api_keys_list = [MY_LIST_OF_POLYGON_API_KEYS]
     response_dict = {}
 
     used_keys = []
@@ -54,10 +21,7 @@ def polygon_check(ticker_list):
             api_key = random.choice(api_keys_list)
             used_keys.append(api_key)
             
-        # print(api_key)
-
         if 'X:' in i:
-            # try:
             api_url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/prev?adjusted=true&apiKey={api_key}'
             data = requests.get(api_url).json()
             response_dict[ticker] = []
@@ -65,11 +29,8 @@ def polygon_check(ticker_list):
             date_timestamp = data['results'][0]['t']
             date_str = datetime.fromtimestamp(date_timestamp // 1000).strftime("%Y-%m-%d %H:%M")
             response_dict[ticker].append(date_str)
-            # except:
-            #     pass
 
         else:
-            # try:
             api_url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/prev?adjusted=true&apiKey={api_key}'
             data = requests.get(api_url).json()
             response_dict[ticker] = []
@@ -77,8 +38,6 @@ def polygon_check(ticker_list):
             date_timestamp = data['results'][0]['t']
             date_str = datetime.fromtimestamp(date_timestamp // 1000).strftime("%Y-%m-%d %H:%M")
             response_dict[ticker].append(date_str)
-            # except:
-            #     pass
 
 
     return response_dict
